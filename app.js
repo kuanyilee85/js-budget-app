7c250d2100c9dd83e52d4e22a77be1df54ff1aae
kuanyilee85 <kuanyilee85@gmail.com>
2019-10-24 21:09:37 +0800

    Adding clear field method to UIController

--- app.js
+++ app.js
@@ -105,6 +105,24 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
 
+    clearFields: function() {
+      var fields, fieldsArr;
+
+      //select fields that we want to clear; but it will give a list, will need to convert to array
+      fields = document.querySelectorAll(
+        DOMstrings.inputDescription + ", " + DOMstrings.inputValue
+      );
+
+      //convert "List" to "Array" by calling slice()
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      //clean the fieldsArr array by forEach()
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = "";
+      });
+      fieldsArr[0].focus();
+    },
+
     // pass the DOMstrings for other "controller" to use, which is expose DOMstrings to public
     getDOMstrings: function() {
       return DOMstrings;
@@ -142,8 +160,11 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     // 3. Add the item to the UI
     UICtrl.addListItem(newItem, input.type);
-    // 4. Calculate the budget
-    // 5. Display the budget on the UI
+
+    // 4. Clear the fields
+    UIController.clearFields();
+    // 5. Calculate the budget
+    // 6. Display the budget on the UI
   };
 
   return {
