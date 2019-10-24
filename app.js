81c0173be0a04ba96a83906a06e6684be2880ac1
kuanyilee85 <kuanyilee85@gmail.com>
2019-10-24 14:04:50 +0800

    organize event listeners and setting up initialization function

--- app.js
+++ app.js
@@ -37,24 +37,36 @@ var UIController = (function() {
 var controller = (function(budgetCtrl, UICtrl) {
   //get DOMstrings from UIController
   var DOM = UICtrl.getDOMstrings();
+
+  var setupEventListeners = function() {
+    // create the event by addEventListener when button was clicked
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+    // create the event by addEventListener when return was pressed
+    document.addEventListener("keypress", function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
   // for DRY, ctrlAddItem is for the function inside btn clicked && key pressed
   var ctrlAddItem = function() {
     // 1. Get the filed input data
     var input = UICtrl.getInput();
-    console.log(input);
     // 2. Add the item to the budgetcontroller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
   };
 
-  // create the event by addEventListener when button was clicked
-  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
-
-  // create the event by addEventListener when return was pressed
-  document.addEventListener("keypress", function(event) {
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+  return {
+    init: function() {
+      console.log("Application has started.");
+      setupEventListeners();
     }
-  });
+  };
 })(budgetController, UIController);
+
+// the only code outside controller, this app start after we call the init()
+controller.init();
