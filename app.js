d8e441562eb9f79fcab84674f52aaa25025de15b
kuanyilee85 <kuanyilee85@gmail.com>
2019-10-25 16:16:45 +0800

    create displayMonth() to UIController for updating current date

--- app.js
+++ app.js
@@ -176,7 +176,8 @@ var UIController = (function() {
     expensesLabel: ".budget__expenses--value",
     percentageLabel: ".budget__expenses--percentage",
     container: ".container",
-    expensesPercLabel: ".item__percentage"
+    expensesPercLabel: ".item__percentage",
+    dateLabel: ".budget__title--month"
   };
 
   var formatNumber = function(num, type) {
@@ -301,6 +302,32 @@ var UIController = (function() {
       });
     },
 
+    displayMonth: function() {
+      var now, year;
+      var now = new Date();
+
+      months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+      month = now.getMonth();
+
+      year = now.getFullYear();
+
+      document.querySelector(DOMstrings.dateLabel).textContent =
+        months[month] + " " + year;
+    },
+
     // pass the DOMstrings for other "controller" to use, which is expose DOMstrings to public
     getDOMstrings: function() {
       return DOMstrings;
@@ -406,6 +433,7 @@ var controller = (function(budgetCtrl, UICtrl) {
   return {
     init: function() {
       console.log("Application has started.");
+      UICtrl.displayMonth();
       //set default number to zero
       UICtrl.displayBudget({
         budget: 0,
